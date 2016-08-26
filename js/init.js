e3098cffcd30227a300521772460c00e79320bf2
Prabhakaran <Prabhakaran@users.noreply.github.com>
2016-08-26 10:18:36 +0530

    Update init.js

--- js/init.js
+++ js/init.js
@@ -6,34 +6,42 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-
-/*$(':input').keydown(function(evt){
-  if(evt.which == "13")
-    {
-
-    }
-})*/
-
-$('#email2').keydown(function(evt){
-  if(evt.which == "13")
-    {
-      gotoStep(4);
-    }
-})
-
-$('#category').keydown(function(evt){
-  if(evt.which == "13")
-    {
-      gotoStep(5);
-
-    }
-})
-
-
-
 function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
+}
+
+function goBack(num){
+  switch(num){
+    case 1:
+      $(".enter-name").slideUp('fast', function(){
+         $(".template-cards").animate({"left":0}, function(){$(this).slideDown('fast')});
+      });
+      $(".step-1").addClass('active-step');
+      $(".step-2").removeClass('active-step');
+      break;
+    case 2:
+      $(".enter-email").slideUp('fast', function(){
+         $(".enter-name").animate({"left":0}, function(){$(this).slideDown('fast')});
+      });
+      $(".step-2").addClass('active-step');
+      $(".step-3").removeClass('active-step');
+      break;
+    case 3:
+      $(".select-category").slideUp('fast', function(){
+         $(".enter-email").animate({"left":0}, function(){$(this).slideDown('fast')});
+      });
+      $(".step-3").addClass('active-step');
+      $(".step-4").removeClass('active-step');
+      break;
+    case 4:
+      $(".bulid-resume").slideUp('fast', function(){
+         $(".select-category").animate({"left":0}, function(){$(this).slideDown('fast')});
+      });
+      $(".step-4").addClass('active-step');
+      $(".step-5").removeClass('active-step');
+      break;
+  }
 }
 
 function gotoStep(num){
@@ -64,25 +72,20 @@ function gotoStep(num){
         }
       	break;
       case 4:
-        if(($("#email2").val().trim().length > 0) && (validateEmail($("#email2").val()))){
+        if(($("#email2").val().trim().length > 0) && validateEmail($("#email2").val())){
         	$(".enter-email").animate({"left":"-100%"}, function(){
             $(this).slideUp('slow', function(){
-              
               $(".select-category").slideDown('fast');
-              $('.enter-email').css('display','none');
             });
           });
-      //$('.enter-email').css('display','none');
+
         	$(".step-3").addClass('step-done');
           $(".step-4").addClass('active-step');
-
        }
       	break;
       case 5:
-
         $(".select-category").animate({"left":"-100%"}, function(){
           $(this).slideUp('slow', function(){
-            $('.enter-email').css('display','none');
             $(".bulid-resume").slideDown('fast', function(){
               fnRenderBuilderResumeComponents();
             });
@@ -91,6 +94,8 @@ function gotoStep(num){
 
         $(".step-4").addClass('step-done');
         $(".step-5").addClass('active-step');
+
+
 
       default: return
     }
@@ -107,9 +112,7 @@ $.validator.setDefaults({
             .attr('data-error', error.text());
     },
     submitHandler: function (form) {
-        //console.log('form ok');
-        //gotoStep(4)
-
+        console.log('form ok');
     }
 });
 
