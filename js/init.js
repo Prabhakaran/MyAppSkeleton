dc074504cb311c1800f8377847ff3f09bb3871b1
Prabhakaran <Prabhakaran@users.noreply.github.com>
2016-09-08 22:02:36 +0530

    Update init.js

--- js/init.js
+++ js/init.js
@@ -84,13 +84,14 @@ function gotoStep(num){
        }
       	break;
       case 5:
-        $(".select-category").animate({"left":"-100%"}, function(){
+        location.href = "profile.html"
+        /*$(".select-category").animate({"left":"-100%"}, function(){
           $(this).slideUp('slow', function(){
             $(".bulid-resume").slideDown('fast', function(){
               fnRenderBuilderResumeComponents();
             });
           });
-        });
+        });*/
 
         $(".step-4").addClass('step-done');
         $(".step-5").addClass('active-step');
@@ -124,39 +125,4 @@ $("#form").validate({
     }
 });
 
-function  fnRenderBuilderResumeComponents(){
-  var $obj = $('.table-of-contents');
-  var top = $obj.offset().top - parseFloat($obj.css('marginTop').replace(/auto/, 0));
 
-  $(window).scroll(function (event) {
-    var y = $(this).scrollTop();
-    if (y >= top) {
-      $obj.addClass('fixed');
-    } else {
-      $obj.removeClass('fixed');
-    }
-  });
-
-  $(".build-section .edit-btn").click(function(event) {
-    $(this).addClass('hide');
-    $(this).parent().find('.viewable').addClass('hide');
-    $(this).parent().find('.editable, .actions').removeClass('hide');
-  });
-
-  $(".build-section .cancel-btn").click(function(event) {
-    $(this).parent().parent().find('.viewable, .edit-btn').removeClass('hide');
-    $(this).parent().parent().find('.editable, .actions').addClass('hide');
-  });
-
-  $('#editor_summary, #editor_school_1, #editor_job_desc').trumbowyg({
-    btns: ['viewHTML',
-    '|', 'formatting',
-    '|', 'btnGrp-design',
-    '|', 'link',
-    '|', 'btnGrp-justify',
-    '|', 'btnGrp-lists',
-    '|', 'horizontalRule'],
-    autogrow:true
-  });
-  
-}
